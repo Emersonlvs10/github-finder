71d9eb387ce3320fa760313b7e5c2db3a00c86d0
Emerson <emersonlvs10@gmail.com>
2023-02-05 11:34:48 -0300

    Projeto final

---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+//pages
+import Home from './routes/Home'
+import Respo from './routes/Repos'
+import Repos from './routes/Repos'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>,
+      },
+      {
+        path:"/repos/:username",
+        element:<Repos/>,
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+   <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
